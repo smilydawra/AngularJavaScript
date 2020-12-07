b2859a6c95cbc6d97cf9e55f4fc5279e30f8d4e7
smilyd <smilydawra92@gmail.com>
2020-12-06 22:36:11 -0500

    functions added for CRUD operations

--- Angular10Crud/src/app/services/tutorial.service.ts
+++ Angular10Crud/src/app/services/tutorial.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import Tutorial from '../models/tutorials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
 
-  constructor() { }
+  private dbPath = '/tutorials';
+
+  tutorialsRef: AngularFireList<Tutorial> = null;
+
+  constructor(private db: AngularFireDatabase) {
+    this.tutorialsRef = db.list(this.dbPath);
+   }
+
+   getAll(): AngularFireList<Tutorial> {
+     return this.tutorialsRef;
+   }
+
+   create(tutorial: Tutorial): any {
+     return this.tutorialsRef.push(tutorial);
+   }
+
+   update(key: string, value: any): Promise<void> {
+     return this.tutorialsRef.update(key, value)
+   }
+
+   delete(key: string): Promise<void> {
+     return this.tutorialsRef.remove(key);
+   }
+
+   deleteAll(): Promise<void> {
+     return this.tutorialsRef.remove();
+   }
 }
