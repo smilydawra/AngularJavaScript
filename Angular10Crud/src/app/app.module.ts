6249eae372804828e28518678345ce2f800737e1
smilyd <smilydawra92@gmail.com>
2020-12-06 21:14:47 -0500

    firebase installed and imports added, environment changes have been made

--- Angular10Crud/src/app/app.module.ts
+++ Angular10Crud/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialsComponent } from './components/add-tutorials/add-tutorials.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialsComponent,
+    TutorialDetailsComponent,
+    TutorialListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular10Crud/src/app/components/add-tutorials/add-tutorials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tutorials',
+  templateUrl: './add-tutorials.component.html',
+  styleUrls: ['./add-tutorials.component.css']
+})
+export class AddTutorialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
