99e6f471ad40668cfaba3d5e9b76323b67dcd952
smilyd <smilydawra92@gmail.com>
2020-12-06 23:39:01 -0500

    CRUD Application completed

--- Angular10Crud/src/app/app-routing.module.ts
+++ Angular10Crud/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
+import { AddTutorialsComponent } from './components/add-tutorials/add-tutorials.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialListComponent },
+  { path: 'add', component: AddTutorialsComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
